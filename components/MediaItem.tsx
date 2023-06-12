c9512485d6d7e5238c0f4e0ef976342f0c3e7629
Tomas Samak <samaktomas@gmail.com>
2023-06-12 15:22:04 +0200

    bug fix

--- components/MediaItem.tsx
+++ components/MediaItem.tsx
@@ -1,5 +1,6 @@
 "use client";
 import useLoadImage from "@/hooks/useLoadImage";
+import usePlayer from "@/hooks/usePlayer";
 import { Song } from "@/types";
 import Image from "next/image";
 import React from "react";
@@ -11,13 +12,14 @@ interface MediaItemProps {
 
 function MediaItem({ data, onClick }: MediaItemProps) {
   const imageUrl = useLoadImage(data);
+  const player = usePlayer();
 
   const handleClick = () => {
     if (onClick) {
       return onClick(data.id);
     }
 
-    // TODO
+    return player.setId(data.id);
   };
 
   return (
